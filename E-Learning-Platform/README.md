50e0ce810967b3d5c89923fd73f66b473d6e92c5
DEVENDRA KUMAWAT <109416405+devendra118@users.noreply.github.com>
2025-04-14 12:54:37 +0530

    Add files via upload

---
+++ E-Learning-Platform/README.md
@@ -0,0 +1,2 @@
+# E-Learning
+This is a E-learning Platform
--- login-js.js
+++ login-js.js
@@ -1,14 +1,15 @@
-$(".signup").css("display", "none");
-
-$(".signbtn").click(function(){
-    $("form").animate({ height:"toggle", opacity: "toggle"}, "slow");
+// Toggle password visibility
+$(document).ready(function() {
+    $(".password-toggle").click(function() {
+        const passwordInput = $(this).closest('.input-group').find('input');
+        const icon = $(this).find('i');
+        
+        if (passwordInput.attr("type") === "password") {
+            passwordInput.attr("type", "text");
+            icon.removeClass('bi-eye').addClass('bi-eye-slash');
+        } else {
+            passwordInput.attr("type", "password");
+            icon.removeClass('bi-eye-slash').addClass('bi-eye');
+        }
+    });
 });
-
-$("#showhide").click(function(){
-var pass = $("#myinput");
-if (pass.attr("type") == "password") {
-pass.attr("type", "text");
-} else {
-pass.attr("type", "password");
-}
-})
